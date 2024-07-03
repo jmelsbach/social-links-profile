27400aa874f2d4eb45fa81c15c5219b9a1db2795
Johannes Melsbach <johannes@melsbach.dev>
2024-07-03 14:10:26 +0200

    Update App.tsx and Button.tsx styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ function App() {
   const menuItems: string[] = ['GitHub', 'Frontend Mentor', 'LinkedIn', 'Twitter', 'Instagram']
   
   return (
-   <main className="flex flex-col items-center justify-center bg-[#1F1F1F] w-min p-6 sm:p-10 rounded-[12px] font-sans">
+   <main className="flex flex-col items-center justify-center bg-[#1F1F1F] w-min p-6 xl:p-10 rounded-[12px] font-sans">
     <img className="rounded-full w-[88px] mb-6" src={profilePicture} alt="profile picture" />
     <h1 className="text-2xl text-white">Jessica Randall</h1>
     <p className="text-sm text-[#C4F82A] font-semibold">London, United Kingdom</p>
     <p className="text-sm text-white m-6 text-center w-[100%]">"Front-end Developer and avid reader."</p>
 
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-4">
       {menuItems.map((item, index) => 
         <Button key={index} text={item}></Button>
       )}
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 function Button({ text }: ButtonProps) {
   return (
-    <button className="h-[45px] w-[279px] sm:w-[304px] rounded-lg bg-[#333333] font-semibold text-white hover:bg-[#C4F82A] hover:text-[#333333]">
+    <button className="h-[45px] w-[279px] xl:w-[304px] rounded-lg bg-[#333333] font-semibold text-white hover:bg-[#C4F82A] hover:text-[#333333]">
       {text}
     </button>
   );
